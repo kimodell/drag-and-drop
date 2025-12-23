eff46911327c4a72f4a955e2070546efc4f1d78f
Kim O'Dell <kimbo.odell@gmail.com>
2025-12-23 18:49:58 -0500

    Refactor to renderPrjects by creating a project items class to render more details from porject class

--- src/app.ts
+++ src/app.ts
@@ -155,6 +155,29 @@ abstract class Componenet<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+
+//Project item class
+//render each project item with title, people and description from project class
+class ProjectItem extends Componenet<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  };
+
+  configure() { }
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 //ProjectList Class
 //render list of projects 
 class ProjectList extends Componenet<HTMLDivElement, HTMLElement>{
@@ -197,9 +220,7 @@ class ProjectList extends Componenet<HTMLDivElement, HTMLElement>{
     )! as HTMLUListElement;
     listEl.innerHTML = '';   //clear existing list items and re-render
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
