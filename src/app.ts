3569107f4b3b3446fa649bfbad4ef11442e9c6d3
Kim O'Dell <kimbo.odell@gmail.com>
2025-12-27 13:55:28 -0500

    refactor and import validation code into seperate namespace file

--- src/app.ts
+++ src/app.ts
@@ -2,45 +2,9 @@
 ///<reference path="drag-drop-interfaces.ts"/> 
 ///<reference path="project-model.ts"/>
 ///<reference path="project-state.ts"/>
+///<reference path="validation.ts"/>
 
 namespace App {
-
-
-  //validation 
-  interface Validatable {
-    value: string | number;
-    required?: boolean;
-    minLength?: number;
-    maxLength?: number;
-    min?: number;
-    max?: number;
-  }
-
-  function validate(validatableInput: Validatable) {
-    let isValid = true;
-    if (validatableInput.required) {
-      isValid = isValid && validatableInput.value.toString().trim().length !== 0;
-    }
-
-    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
-      isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
-    }
-
-    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
-      isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
-    }
-
-    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
-      isValid = isValid && validatableInput.value >= validatableInput.min;
-    }
-
-    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
-      isValid = isValid && validatableInput.value < validatableInput.max;
-    }
-
-    return isValid;
-  }
-
   //autobind decorator
   //ensures this always refers to the correct class instance
   function autobind(
