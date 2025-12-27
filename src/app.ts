bf6bea01b1ca3e3d1d4246b366fe55bd0e09f3a2
Kim O'Dell <kimbo.odell@gmail.com>
2025-12-27 14:29:02 -0500

    move imports from app.ts to files that uselize them directly.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,5 @@
 //ts import syntax
-///<reference path="models/drag-drop.ts"/> 
-///<reference path="models/project.ts"/>
-///<reference path="state/project-state.ts"/>
-///<reference path="util/validation.ts"/>
-///<reference path="decorators/autobind.ts"/>
 ///<reference path="components/project-input.ts"/>
-///<reference path="components/project-item.ts"/>
 ///<reference path="components/project-list.ts"/>
 
 
